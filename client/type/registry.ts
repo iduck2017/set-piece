6c5dce156cfcb90c2d399a2f771dee2e1b33b74c
zt.huang <2654427647@qq.com>
2024-09-01 16:56:23 +0800

    feat: refactor

---
+++ client/type/registry.ts
@@ -0,0 +1,18 @@
+/** 模型定义 */
+export enum ModelKey {
+    Code = 'code',
+    State = 'state',
+    Preset = 'preset',
+    Parent = 'parent',
+    ChildList = 'childList',
+    ChildDict = 'childDict',
+    EmitterEventDict = 'emitterEventDict',
+    HandlerEventDict = 'handlerEventDict'
+}
+
+/** 模型注册表 */
+export enum ModelCode {
+    Bunny = 'bunny',
+    Root = 'root',
+    Time = 'time'
+}
