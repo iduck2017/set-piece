5e222ee76eca98243c93f9812ff96f1db48367a4
MaChau <2654427647@qq.com>
2024-10-05 21:02:25 +0800

    chore: refactor

---
+++ client/type/model-tmpl.ts
@@ -0,0 +1,40 @@
+import { Base, Override } from ".";
+import type { App } from "../app";
+import type { Model } from "../models";
+import type { ModelCode } from "../services/factory";
+
+// 模型层节点定义
+export type ModelTmpl = {
+    code: ModelCode
+    info: Base.Data
+    parent: Model | App;
+    childList: Array<ModelTmpl>
+    childDict: Record<Base.Key, ModelTmpl>,
+    eventDict: Base.Dict,
+    effectDict: Base.Dict,
+}
+
+export namespace ModelTmpl {
+    // 基础参数反射
+    export type Code<M extends ModelTmpl> = M['code']
+    export type Info<M extends ModelTmpl> = M['info']
+    export type Parent<M extends ModelTmpl> = M['parent']
+    export type ChildDict<M extends ModelTmpl> = M['childDict']
+    export type ChildList<M extends ModelTmpl> = M['childList']
+    export type EventDict<M extends ModelTmpl> = M['eventDict']
+    export type EffectDict<M extends ModelTmpl> = M['effectDict']
+}
+
+// 自定义模型层节点定义
+export type SpecModelTmpl<
+    M extends Partial<ModelTmpl>
+> = Readonly<Override<{
+    // 空模型层节点定义
+    code: never,
+    info: Base.VoidData,
+    parent: Model,
+    childList: Base.VoidList,
+    childDict: Base.VoidData
+    eventDict: Base.VoidData,
+    effectDict: Base.VoidData,
+}, M>>
