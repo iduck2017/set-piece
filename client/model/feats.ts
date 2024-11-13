e0804ace4522d0e4769e8d82ae064020026d9241
zt.huang <2654427647@qq.com>
2024-11-14 01:43:46 +0800

    feat: inherent

---
+++ client/model/feats.ts
@@ -0,0 +1,87 @@
+import { Def } from "@/type/define";
+import { Model } from ".";
+import { Base } from "@/type/base";
+
+export abstract class Feat<M extends Partial<Def>> extends Model<{
+    tempState: {
+        readonly name: string;
+        readonly desc: string
+    }
+} & M> {}
+
+
+@Model.useProduct('edible')
+export class Edible extends Feat<{
+    type: 'edible';
+    memoState: {
+    },
+    tempState: {
+    },
+}> {
+    constructor(
+        seq: Model.Seq<Edible>,
+        parent: Model.Parent<Edible>
+    ) {
+        super({
+            ...seq,
+            childDict: {},
+            childList: {},
+            memoState: {
+                ...seq.memoState
+            },
+            tempState: {
+                name: 'Edible',
+                desc: ''
+            }
+        }, parent);
+    }
+
+    kill() {
+        console.log('eat');
+    }
+}
+
+interface PetableInfo {
+    readonly rawObedience: number;
+    readonly rawIntelligence: number;
+}
+@Model.useProduct('petable')
+export class Petable extends Feat<{
+    type: 'petable';
+    memoState: {
+    },                
+    tempState: PetableInfo,
+}> {
+    private static readonly _rules: Map<Function, PetableInfo> = new Map();
+    static useRule(info: PetableInfo) {
+        return function (target: Base.Class<Model>) {
+            Petable._rules.set(target, info);
+        };
+    }
+
+    constructor(
+        seq: Model.Seq<Petable>,
+        parent: Model.Parent<Petable>
+    ) {
+        const rule = Petable._rules.get(parent.constructor);
+        super({
+            ...seq,
+            childDict: {},
+            childList: {},
+            memoState: {
+                ...seq.memoState
+            },
+            tempState: {
+                rawObedience: rule?.rawObedience || 50,
+                rawIntelligence: rule?.rawIntelligence || 50,
+                name: 'Petable',
+                desc: ''
+            }
+        }, parent);
+    }
+
+    play() {
+        console.log('play');
+    }
+}
+
