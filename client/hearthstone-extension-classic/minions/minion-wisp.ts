0b49a53ac0f581f430f44b3a9b761ad277886ded
zt.huang <2654427647@qq.com>
2024-12-19 21:41:29 +0800

    chore: refactor

---
+++ client/hearthstone-extension-classic/minions/minion-wisp.ts
@@ -0,0 +1,29 @@
+import { CustomDef, Def, Factory, Props } from "@/set-piece";
+import { MinionDef, MinionModel } from "@/hearthstone/models/minion";
+
+export type WispDef = MinionDef<
+    CustomDef<{
+        code: 'minion-wisp',
+    }>
+>
+
+@MinionModel.useRule({
+    manaCost: 0,
+    health: 1,
+    attack: 1,
+    races: []
+})
+@Factory.useProduct('minion-wisp') 
+export class WispModel extends MinionModel<WispDef> {
+    constructor(props: Props<WispDef>) {
+        const superProps = MinionModel.minionProps(props);
+        super({
+            ...superProps,
+            paramDict: {
+                name: 'Wisp',
+                desc: ''
+            },
+            stateDict: {}
+        });
+    }
+}
