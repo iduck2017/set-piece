51c3bcfe7e2c36a061f0d6d23f036822ed78a5db
MaChau <2654427647@qq.com>
2025-03-16 14:20:15 +0800

    feat: draft

---
+++ src/demo/human.ts
@@ -0,0 +1,89 @@
+import { OnChildChange } from "../model/event";
+import { Model } from "../model/model";
+import { OuterPartyDefine } from "./outer-party";
+import { PetModel } from "./pet";
+
+export namespace HumanDefine {
+    export type I = string | OuterPartyDefine.I;
+    export type E = { onBorn: void, onSpeek: string, onHello: HumanModel, onCount: number }
+    export type S1 = { nickname?: string, emotion: string }
+    export type S2 = { isAlive: boolean, age: number, name: string }
+    export type P = HumanModel
+    export type C1 = { cat: PetModel }
+    export type C2 = HumanModel
+    export type R1 = { mother: HumanModel }
+    export type R2 = { friends: HumanModel }
+}
+
+export  class HumanModel<
+    I extends HumanDefine.I = HumanDefine.I,
+    E extends Partial<HumanDefine.E> = {},
+    S1 extends Partial<HumanDefine.S1> = {},
+    S2 extends Partial<HumanDefine.S2> = {},
+    P extends HumanDefine.P = HumanDefine.P,
+    C1 extends Partial<HumanDefine.C1> = {},
+    C2 extends HumanDefine.C2 = HumanDefine.C2,
+    R1 extends Partial<HumanDefine.R1> = {},
+    R2 extends Partial<HumanDefine.R2> = {}
+> extends Model<
+    I,
+    E & HumanDefine.E,
+    S1 & HumanDefine.S1,
+    S2 & HumanDefine.S2,
+    P,
+    C1 & HumanDefine.C1,
+    C2,
+    R1 & HumanDefine.R1,
+    R2 & HumanDefine.R2
+> {
+    static superProps<M extends HumanModel>(props: Model.Props<M>) {
+        return {
+            ...props,
+            state: { ...props.state, isAlive: true, age: 20 },
+        }
+    }
+    
+    test() {
+        const human: HumanModel = this;
+        const age: number = human.state.age;
+        const isAlive: boolean = human.state.isAlive;
+        const name: string = human.state.name;
+        const nickname: string = human.state.nickname ?? '';
+        const emotion: string = human.state.emotion;
+        human.stateDelegator.age += 10;
+        human.stateDelegator.emotion = 'happy';
+        human.stateDelegator.isAlive = true;
+        human.stateDelegator.nickname = '';
+        this.eventEmitters.onBorn(undefined);
+        this.eventEmitters.onSpeek('Hello');
+        this.eventEmitters.onCount(4);
+        this.eventEmitters.onHello(this.parent);
+        const cat: PetModel | undefined = this.child.cat;
+        this.childDelegator.cat?.state?.age;
+        this.child.cat?.state.age;
+        this.childDelegator.cat = { code: 'pet' };
+        const employee: HumanModel = this.child[0];
+        this.childDelegator.push({ code: 'thinkpol', state: { isAlive: false } });
+        const mother: HumanModel | undefined= this.refer.mother;
+        const friends: HumanModel | undefined = this.refer.friends?.[0];
+        this.referDelegator.mother;
+        this.eventEmitters.onCount(4)
+    }
+    
+    @Model.useDecor((model: HumanModel) => model.decor.nickname)
+    onNicknameCheck(target: HumanModel, state: string) { return state }
+
+    @Model.useEvent((model) => model.event.onHello)
+    onBorn(target: HumanModel, event: HumanModel) {}
+
+    @Model.useEvent((model) => model.agent.event.onHello)
+    onBorn2(target: HumanModel, to: HumanModel) {}
+
+    @Model.useEvent((model: HumanModel) => model.agent.child[0].event.onHello)
+    onBorn3(target: HumanModel, to: HumanModel) {}
+
+    @Model.useEvent((model: HumanModel) => model.event.onChildChange)
+    onChildChange(target: HumanModel, event: OnChildChange<HumanModel>) {
+        console.log(event.next, event.prev)
+    }
+}
---
+++ src/demo/thinkpol.ts
@@ -0,0 +1,70 @@
+import { HumanModel } from "./human"
+import { Model } from "../model/model"
+import { OuterPartyDefine, OuterPartyModel } from "./outer-party"
+import { PetModel } from "./pet"
+
+export namespace ThinkPolDefine {
+    export type I = 'thinkpol'
+    export type E = Partial<OuterPartyDefine.E> & { onSpy: OuterPartyModel }
+    export type S1 = Partial<OuterPartyDefine.S1> & { alias: string }
+    export type S2 = Partial<OuterPartyDefine.S2>
+    export type P = OuterPartyDefine.P
+    export type C1 = Partial<OuterPartyDefine.C1> & { dog: PetModel }
+    export type C2 = OuterPartyDefine.C2
+    export type R1 = Partial<OuterPartyDefine.R1> & { target: OuterPartyModel }
+    export type R2 = Partial<OuterPartyDefine.R2>
+}
+
+export class ThinkPolModel extends OuterPartyModel<
+    ThinkPolDefine.I,
+    ThinkPolDefine.E,
+    ThinkPolDefine.S1,
+    ThinkPolDefine.S2,
+    ThinkPolDefine.P,
+    ThinkPolDefine.C1,
+    ThinkPolDefine.C2,
+    ThinkPolDefine.R1,
+    ThinkPolDefine.R2
+> {
+    constructor(props: Model.Props<ThinkPolModel>) {
+        const superProps = OuterPartyModel.superProps(props);
+        super({
+            ...props,
+            state: {
+                ...superProps.state,
+                alias: 'thinkpol',
+                salary: 10000,
+                emotion: 'happy',
+                seniority: 1,
+                name: 'thinkpol',
+            },
+            child: {
+                ...superProps.child,
+                dog: { code: 'pet' }
+            }
+        });
+    }
+
+    test() {
+        const dog: PetModel = this.child.dog;
+        this.childDelegator.dog = { code: 'pet' };
+        this.child.cat?.state.age;
+        const model: Model = this;
+        const outerParty: OuterPartyModel = this;
+        const human: HumanModel = this
+        this.eventEmitters.onHello(human);
+        this.eventEmitters.onSpy(this);
+        this.eventEmitters.onBorn();
+        const cat: PetModel = this.child.cat;
+    }
+
+    
+    @Model.useEvent((model) => model.event.onCount)
+    onBorn4(target: HumanModel, event: number) {}
+
+    @Model.useEvent((model) => model.agent.event.onHello)
+    onBorn5(target: HumanModel, to: HumanModel) {}
+
+    @Model.useEvent((model: HumanModel) => model.agent.child[0].event.onHello)
+    onBorn6(target: HumanModel, to: HumanModel) {}
+}
